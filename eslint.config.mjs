1c52e9b4c75fd9f729fcdc3ffae48f62b595ae3c
Yuriy Pilipenko <yurik2061@gmail.com>
2025-03-15 18:40:44 +0200

    config ts & lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,30 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Allow unused variables if they start with `_`
+      "@typescript-eslint/no-unused-vars": [
+        "warn", // Set to "warn" to show warnings, or "off" to disable completely
+        {
+          vars: "all",
+          args: "after-used",
+          ignoreRestSiblings: true,
+          varsIgnorePattern: "^_", // Ignore unused variables starting with `_`
+          argsIgnorePattern: "^_", // Ignore unused arguments starting with `_`
+        },
+      ],
+      // Avoid "any" type usage but allow controlled exceptions
+      "@typescript-eslint/no-explicit-any": [
+        "warn", // Warn instead of erroring
+        {
+          fixToUnknown: true, // Suggest `unknown` instead of `any`
+          ignoreRestArgs: false, // Disallow `any` in rest arguments
+        },
+      ],
+
+    },
+  },
 ];
 
 export default eslintConfig;
--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,8 +3,8 @@ import { DefaultSession } from "next-auth";
 declare module "next-auth" {
   interface Session {
     user: {
-      role: string;
       id: string;
+      role: string;
     } & DefaultSession["user"];
   }
 
