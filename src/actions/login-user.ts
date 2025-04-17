3c4e640c8ba55ef2c822c0baabbb0a05a7cd090d
Yuriy Pilipenko <yurik2061@gmail.com>
2025-04-17 15:20:46 +0300

    loginUser template

---
+++ src/actions/login-user.ts
@@ -0,0 +1,12 @@
+'use server'
+
+import { connectDB } from '@/lib/db';
+import User from '@/models/User';
+import { hash } from 'bcryptjs';
+import { revalidatePath } from "next/cache";
+
+export async function loginUser (formData: FormData)  {
+
+  const email = formData.get('email') as string | null;
+  const password = formData.get('password') as string | null;
+}
