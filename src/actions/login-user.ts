0cf4c2c5c0999822d7465fb4921af63cb25454ba
Yuriy Pilipenko <yurik2061@gmail.com>
2025-04-17 15:21:16 +0300

    all fields check

--- src/actions/login-user.ts
+++ src/actions/login-user.ts
@@ -9,4 +9,7 @@ export async function loginUser (formData: FormData)  {
 
   const email = formData.get('email') as string | null;
   const password = formData.get('password') as string | null;
+  if ( !email || !password) {
+    return { success: false, error: "requiredFields" };
+  }
 }
