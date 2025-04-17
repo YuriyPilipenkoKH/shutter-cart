569487230dae00f916bd051a29203b30891566aa
Yuriy Pilipenko <yurik2061@gmail.com>
2025-04-17 15:08:16 +0300

    hookform applied

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,6 +1,23 @@
+import { LogInput, loginSchema } from '@/models/RegisterSchema'
+import { zodResolver } from '@hookform/resolvers/zod'
 import React from 'react'
+import { useForm } from 'react-hook-form'
 
 const LoginForm = () => {
+    const {
+      register, 
+      handleSubmit,
+      formState,
+      reset,
+  
+    } = useForm<LogInput>({
+      defaultValues: {
+        email: '',
+        password: '',
+      },
+        mode:'onBlur',
+        resolver: zodResolver(loginSchema),
+    })
   return (
     <div>LoginForm</div>
   )
